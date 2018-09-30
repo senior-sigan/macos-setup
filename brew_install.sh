2a232697c117bc3445355ed14be6e431ca8b28db
Ilya Siganov <ilya.blan4@gmail.com>
2018-09-30 17:21:50 +0600

    add some programs to brew script

---
+++ brew_install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+brew tap caskroom/cask
+
+brew install mc
+
+brew cask install spotify
+brew cask install spotifree
+brew cask install vlc
+brew cask install webtorrent
+brew install youtube-dl
+
+brew cask install google-chrome
+
+brew cask install jetbrains-toolbox
+brew cask install unity-hub
+brew cask install sublime-text
+brew cask install atom
+brew cask install visual-studio-code
+brew cask install virtualbox
+
+brew cask install rocket # emojii picker
+brew cask install punto-switcher
+brew cask install alfred # spotlight replacement
+brew cask install the-unarchiver # rar, p7zip and so on
+#brew cask install keka
+brew cask install smcfancontrol # fan control
+brew cask install itsycal # toolbox calendar
+brew cask install adobe-acrobat-reader
+brew cask install skitch # screenshoter and editor
+brew cask install gimp
+brew cask install spectacle # window management
+brew cask install cheatsheet
+
+brew cask install paragon-ntfs
+brew cask install aerial
+
+brew cask install skype
+brew cask install telegram-desktop
+brew cask install slack
+
+brew cask install evernote
+
+brew cask install steam
+
+brew cask install yandex-disk
+brew cask install dropbox
+brew cask install google-photos-backup-and-sync
+
+brew cask install java
+brew install pyenv
