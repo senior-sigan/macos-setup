1939520678d7b4062e435c14f74478a105437b5b
Ilya Siganov <ilya.blan4@gmail.com>
2019-03-07 15:14:34 +0600

    update

--- brew_install.sh
+++ brew_install.sh
@@ -5,7 +5,6 @@ brew tap caskroom/cask
 brew install mc
 
 brew cask install spotify
-brew cask install spotifree
 brew cask install vlc
 brew cask install clementine # music player
 brew cask install webtorrent
@@ -22,9 +21,11 @@ brew cask install atom
 brew cask install visual-studio-code
 brew cask install virtualbox
 brew cask install postman
-brew cask install obs
 
-brew cask install rocket # emojii picker
+brew cask install obs
+brew cask install soundflower
+
+brew cask install rocket # emojii picker https://matthewpalmer.net/rocket/
 brew cask install punto-switcher
 brew cask install alfred # spotlight replacement
 brew cask install the-unarchiver # rar, p7zip and so on
@@ -32,8 +33,10 @@ brew cask install the-unarchiver # rar, p7zip and so on
 brew cask install smcfancontrol # fan control
 brew cask install itsycal # toolbox calendar
 brew cask install adobe-acrobat-reader
+brew cask install djview
 brew cask install skitch # screenshoter and editor
 brew cask install gimp
+# brew cask install paintbrush # windows paint
 brew cask install spectacle # window management
 brew cask install cheatsheet
 
@@ -49,8 +52,10 @@ brew cask install flux
 brew cask install evernote
 brew cask install notion
 brew cask install raindropio
+brew cask install dynalist
 
 brew cask install steam
+brew cask install openemu
 
 brew cask install yandex-disk
 brew cask install dropbox
@@ -64,4 +69,7 @@ brew install cmake
 
 # machine learning
 brew install pyenv
-brew cask install nteract
+brew cask install nteract  # jupyter notebook app
+
+# brew cask install macmediakeyforwarder  # to fix media keys bug
+brew cask install balenaetcher # to create ubuntu usb stick
