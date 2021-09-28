3cbb7c9729e27a8ea3f857926d30816da87299f4
Илья Сиганов <ilya@Ilas-MacBook-Pro.local>
2021-09-28 20:27:11 +0600

    update

--- brew_install.sh
+++ brew_install.sh
@@ -2,73 +2,74 @@
 
 brew tap homebrew/cask-fonts
 
+brew install iterm2
 brew install mc
 
-brew cask install google-chrome
+brew install --cask google-chrome
 
-brew cask install skype
-brew cask install telegram-desktop
-brew cask install slack
-brew cask install discord
+brew install --cask skype
+brew install --cask telegram-desktop
+brew install --cask slack
+brew install --cask discord
 
-brew cask install spotify
-brew cask install vlc
-brew cask install transmission # torrent tracker
+brew install --cask spotify
+brew install --cask vlc
+brew install --cask transmission # torrent tracker
 brew install youtube-dl
 
-brew cask install devdocs
-brew cask install jetbrains-toolbox
-brew cask install unity-hub
-brew cask install sublime-text
-brew cask install visual-studio-code
-brew cask install virtualbox
-brew cask install postman
+brew install --cask devdocs
+brew install --cask jetbrains-toolbox
+brew install --cask unity-hub
+brew install --cask sublime-text
+brew install --cask visual-studio-code
+brew install --cask virtualbox
+brew install --cask postman
 
-brew cask install obs # streaming
-brew cask install soundflower # macos audio tricks
+brew install --cask obs # streaming
+brew install --cask soundflower # macos audio tricks
 
-#brew cask install rocket # emojii picker https://matthewpalmer.net/rocket/
-#brew cask install punto-switcher
-#brew cask install alfred # spotlight replacement
-brew cask install clipy # clipboard client https://github.com/Clipy/Clipy
-brew cask install the-unarchiver # rar, p7zip and so on
-brew cask install smcfancontrol # fan control
-brew cask install itsycal # toolbox calendar
-brew cask install adobe-acrobat-reader
-brew cask install djview
-brew cask install tunnelblick # vpn
-#brew cask install skitch # screenshoter and editor
-brew cask install gimp
-brew cask install spectacle # window management
-brew cask install dozer # hide mac os menubar
-brew cask install cheatsheet # show hotkeys while press cmd
-brew cask install time-lapse-assembler
-brew cask install iriunwebcam # use iphone as a webcam
-brew cask install vnc-viewer
-brew cask install android-file-transfer
+#brew install --cask rocket # emojii picker https://matthewpalmer.net/rocket/
+#brew install --cask punto-switcher
+#brew install --cask alfred # spotlight replacement
+brew install --cask clipy # clipboard client https://github.com/Clipy/Clipy
+brew install --cask the-unarchiver # rar, p7zip and so on
+brew install --cask smcfancontrol # fan control
+brew install --cask itsycal # toolbox calendar
+brew install --cask adobe-acrobat-reader
+brew install --cask djview
+brew install --cask tunnelblick # vpn
+#brew install --cask skitch # screenshoter and editor
+brew install --cask gimp
+brew install --cask spectacle # window management
+brew install --cask dozer # hide mac os menubar
+brew install --cask cheatsheet # show hotkeys while press cmd
+brew install --cask time-lapse-assembler
+brew install --cask iriunwebcam # use iphone as a webcam
+brew install --cask vnc-viewer
+brew install --cask android-file-transfer
 
-brew cask install toggl # time tracker
+brew install --cask toggl # time tracker
 
-brew cask install paragon-ntfs
-brew cask install aerial # apple tv wallpapers
+brew install --cask paragon-ntfs
+brew install --cask aerial # apple tv wallpapers
 
-# brew cask install flux
+# brew install --cask flux
 
 ### Notes, lists, bookmarks trackers
-brew cask install evernote
-brew cask install notion
-brew cask install raindropio
-brew cask install dynalist
-brew cask install zotero # academic bookmarks manager
+brew install --cask evernote
+brew install --cask notion
+brew install --cask raindropio
+brew install --cask dynalist
+brew install --cask zotero # academic bookmarks manager
 
-brew cask install steam
-#brew cask install openemu
+brew install --cask steam
+#brew install --cask openemu
 
-brew cask install yandex-disk
-brew cask install dropbox
-brew cask install google-photos-backup-and-sync
+brew install --cask yandex-disk
+brew install --cask dropbox
+brew install --cask google-photos-backup-and-sync
 
-brew cask install java
+brew install --cask java
 
 brew install git-lfs
 
@@ -78,16 +79,18 @@ brew install cmake
 
 # python coding
 brew install pyenv
-brew cask install miniconda
-brew cask install nteract  # jupyter notebook app
+brew install --cask miniconda
+brew install --cask nteract  # jupyter notebook app
 
-# brew cask install macmediakeyforwarder  # to fix media keys bug
-brew cask install balenaetcher # to create ubuntu usb stick
+# brew install --cask macmediakeyforwarder  # to fix media keys bug
+brew install --cask balenaetcher # to create ubuntu usb stick
 
-brew cask install font-fira-code
+brew install --cask font-fira-code
 
 brew install mkvtoolnix # app to merge, modify MKV video files
 
 brew install antigen # zsh plugin manager
 
-# brew cask install vagrant # virtual box management
+# brew install --cask vagrant # virtual box management
+brew install ffmpeg
+
